568362ddb0b71caa9d51fe83f8c9c88b9374ff75
harkiran7237 <harkiran7237@gmail.com>
2020-06-02 08:44:32 +0530

    c

--- js/player.js
+++ js/player.js
@@ -1,22 +1,23 @@
-class player{
-    constructor(){
+class Player {
+  constructor(){}
 
-    }
-    getCount(){
-        var playerCountref=database.ref("playerCount");
-        playerCountref.on("value",function(data){
-            playerCount=data.val();
-        })
-    }
-    updateCount(count){
-        database.ref("/").update({
-            playerCount:count
-        })
-    }
-    update(name){
-        var playerIndex= "player"+playerCount;
-        database.ref(playerIndex).set({
-            Name:name
-        })
-    }
+  getCount(){
+    var playerCountRef = database.ref('playerCount');
+    playerCountRef.on("value",function(data){
+      playerCount = data.val();
+    })
+  }
+
+  updateCount(count){
+    database.ref('/').update({
+      playerCount: count
+    });
+  }
+
+  update(name){
+    var playerIndex = "player" + playerCount;
+    database.ref(playerIndex).set({
+      name:name
+    });
+  }
 }
