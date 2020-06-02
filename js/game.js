b6d53b09b1f21b6d91e320b4a2b629fa793d40ff
harkiran7237 <harkiran7237@gmail.com>
2020-06-02 08:44:21 +0530

    b

--- js/game.js
+++ js/game.js
@@ -1,27 +1,26 @@
-class game{
-    constructor(){
-
-    }
+class Game {
+    constructor(){}
+    
     getState(){
-        var gameStateref=database.ref("gameState");
-        gameStateref.on("value",function(data){
-            gameState=data.val()
-
-        })
-
-
+      var gameStateRef  = database.ref('gameState');
+      gameStateRef.on("value",function(data){
+         gameState = data.val();
+      })
+     
     }
+  
     update(state){
-        database.ref("/").update({
-            gameState:state
-        })
+      database.ref('/').update({
+        gameState: state
+      });
     }
+  
     start(){
-        if(gameState===0){
-            player=new player();
-            player.getCount();
-            form1= new form();
-            form1.display();
-        }
+      if(gameState === 0){
+        player = new Player();
+        player.getCount();
+        form = new Form()
+        form.display();
+      }
     }
-}
+  }
